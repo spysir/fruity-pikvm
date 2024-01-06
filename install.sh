72b0bbd4e82b686991849f0d648614fa2457fc91
spysir <59646715+spysir@users.noreply.github.com>
2024-01-07 00:12:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 
 compile-python(){
 
-  sudo apt update
-  sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev libffi-dev
+  apt update
+  apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev libffi-dev
   
   cd /tmp/
   wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
   tar xzf Python-3.10.9.tgz
   cd Python-3.10.9
 
-  sudo make clean
-  sudo ./configure --enable-optimizations
-  sudo make -j "$(nproc)"
-  sudo make altinstall
-  sudo rm /tmp/Python-3.10.9.tgz
+  make clean
+  ./configure --enable-optimizations
+  make -j "$(nproc)"
+  make altinstall
+  rm /tmp/Python-3.10.9.tgz
 
 }
 
@@ -80,13 +80,13 @@ esac
 DEBFILE="fruity-pikvm_0.2_$ARCHN.deb"
 DEBURL="https://github.com/jacobbar/fruity-pikvm/releases/download/debfile/"
 
-sudo apt update
-sudo apt install -y nginx tesseract-ocr tesseract-ocr-eng janus libevent-dev libgpiod-dev
+apt update
+apt install -y nginx tesseract-ocr tesseract-ocr-eng janus libevent-dev libgpiod-dev
 cd  /tmp
 echo "-->> Downloding fruity-pikvm package"
 wget "$DEBURL$DEBFILE"
 echo "-->> installing fruity-pikvm" 
 dpkg -i $DEBFILE
 rm  $DEBFILE
-sudo kvmd -m 
+kvmd -m 
 echo "reboot and enjoy"
